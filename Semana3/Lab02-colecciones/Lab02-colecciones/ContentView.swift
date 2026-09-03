4995a1c30458c301b9edd51ce93518105d465b47
rivera-anderson <anderson.rivera.p@tecsup.edu.pe>
2026-09-03 13:03:39 -0500

    feat: Crear proyecto Lab03 y Playground en Semana03

---
+++ Semana3/Lab02-colecciones/Lab02-colecciones/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab02-colecciones
+//
+//  Created by Tecsup on 3/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Semana3/Lab02-colecciones/Lab02-colecciones/Lab02_coleccionesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab02_coleccionesApp.swift
+//  Lab02-colecciones
+//
+//  Created by Tecsup on 3/09/26.
+//
+
+import SwiftUI
+
+@main
+struct Lab02_coleccionesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
